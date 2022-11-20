6e9538f14bcad934462eebd3ca030292ea39d6a3
Darren Ng <darrenng@Darrens-Laptop.local>
2022-11-20 22:33:35 +0800

    check if darkmode then change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Navbar } from '../components/Navbar'
 import { BackgroundVideo } from '../components/BackgroundVideo'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isDarkMode,setIsDarkMode] = useState(true);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if(matched)
+          setIsDarkMode(true);
+        else
+          setIsDarkMode(false);
+    }
+  },[])
+      
+
   return (
   <div style={{position:"fixed"}}>
+    {isDarkMode ? "dark mode" : "light mode"}
+    <Head>
     <title>Seraph Labs</title>
+    <link rel="icon" href={isDarkMode ? "/favicon.ico" : "/faviconLight.png"} />
+    </Head>
   <Navbar/>
   <BackgroundVideo/>
   <Component {...pageProps} />
