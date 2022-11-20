2e454476a5be217036b83925710b6db9ede3cf46
Darren Ng <darrenng@Darrens-Laptop.local>
2022-11-20 14:16:57 +0800

    testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { BackgroundVideo } from '../components/BackgroundVideo'
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <div style={{position:"fixed"}}>
+    <title>Seraph Labs</title>
   <Navbar/>
   <BackgroundVideo/>
   <Component {...pageProps} />
