383cc400123a1fcbd2be543c8056df756964d562
darrenbao1 <dbao@seraphlabs.io>
2022-11-29 11:24:19 +0800

    testing speed

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Background.module.css";
 export const Background = () => {
     return (
         <div className={styles.backgroundContainer}>
-            <picture><img src="/background.png"></img></picture>
+            <picture><img src="/background.jpg"></img></picture>
         </div>
     )
 }
