929849af242155558328a9f320b9b2e5b57a75b7
dbao <dbao@seraphlabs.io>
2023-05-16 23:24:17 +0800

    changed order of navbar

--- types/constants.tsx
+++ types/constants.tsx
@@ -1,31 +1,31 @@
 import { page } from "./interfaces";
 
 export abstract class Constants {
-    static readonly ACTIVE_PAGES:page[] = [
-        {
-            title:"Products",
-            routeLink:"/products",
-            isActive: true,
-        },
-        {
-            title:"Team",
-            routeLink:"team",
-            isActive:true,
-        },
-        {
-            title:"Articles",
-            routeLink:"/articles",
-            isActive: true,
-        },
-        {
-            title:"Vision",
-            routeLink: "/vision",
-            isActive: false,
-        },
-        {
-            title:"Advisors",
-            routeLink:"/advisors",
-            isActive:true,
-        },
-    ]
+	static readonly ACTIVE_PAGES: page[] = [
+		{
+			title: "Team",
+			routeLink: "team",
+			isActive: true,
+		},
+		{
+			title: "Advisors",
+			routeLink: "/advisors",
+			isActive: true,
+		},
+		{
+			title: "Products",
+			routeLink: "/products",
+			isActive: true,
+		},
+		{
+			title: "Articles",
+			routeLink: "/articles",
+			isActive: true,
+		},
+		{
+			title: "Vision",
+			routeLink: "/vision",
+			isActive: false,
+		},
+	];
 }
